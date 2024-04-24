1e25e3959788070fa271c5984643843bb9645192
Aja Singer <ajasinger@gmail.com>
2024-04-24 14:07:29 -0400

    add ids

--- lib/data.ts
+++ lib/data.ts
@@ -1,18 +1,22 @@
 export const links = [
     {
         name:"ABOUT ME",
+        id:1,
         hash:"#about"
     },
     {
         name:"WORK",
+        id:2,
         hash:"#work"
     },
     {
         name:"LINKEDIN",
+        id:3,
         link:"https://www.linkedin.com/in/ajasinger/"
     },
     {
         name:"CONTACT ME",
+        id:4,
         hash:"#contact"
     },
 ]
