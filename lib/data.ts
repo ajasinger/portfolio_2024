3dcf491389235bdd1c50ae4d6a5d05d7313c2600
Aja Singer <ajasinger@gmail.com>
2024-04-24 14:13:33 -0400

    change hash to href

--- lib/data.ts
+++ lib/data.ts
@@ -2,21 +2,21 @@ export const links = [
     {
         name:"ABOUT ME",
         id:1,
-        hash:"#about"
+        href:"#about"
     },
     {
         name:"WORK",
         id:2,
-        hash:"#work"
+        href:"#work"
     },
     {
         name:"LINKEDIN",
         id:3,
-        link:"https://www.linkedin.com/in/ajasinger/"
+        href:"https://www.linkedin.com/in/ajasinger/"
     },
     {
         name:"CONTACT ME",
         id:4,
-        hash:"#contact"
+        href:"#contact"
     },
 ]
