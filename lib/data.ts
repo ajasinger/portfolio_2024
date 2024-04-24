bdd6ccd7c283e7dcacde4d68f5dc5178aa3bc965
Aja Singer <ajasinger@gmail.com>
2024-04-24 14:04:20 -0400

    add nav array

---
+++ lib/data.ts
@@ -0,0 +1,18 @@
+export const links = [
+    {
+        name:"ABOUT ME",
+        hash:"#about"
+    },
+    {
+        name:"WORK",
+        hash:"#work"
+    },
+    {
+        name:"LINKEDIN",
+        link:"https://www.linkedin.com/in/ajasinger/"
+    },
+    {
+        name:"CONTACT ME",
+        hash:"#contact"
+    },
+]
