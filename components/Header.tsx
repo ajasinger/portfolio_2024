45b279b56d860e14983e617dbf2d82a986cfe075
Aja Singer <ajasinger@gmail.com>
2024-04-27 07:36:05 -0400

    code and style component

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,8 +1,12 @@
 
-
 export default function Header() {
   return (
-    <header className="font-sans text-lg py-8">
+    <header className="py-16 flex flex-col gap-10">
+      <h1 className="text-[250px] font-semibold">Hi, I'm Aja</h1>
+      <h2 className="text-3xl font-thin max-w-4xl leading-relaxed">
+        a junior<span className="font-serif"> full-stack engineer </span>building at the intersection of 
+        <span className="font-serif"> technology </span>and<span className="font-serif"> creativity</span>.
+      </h2>
       
     </header>
   )
