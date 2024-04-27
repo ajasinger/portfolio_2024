539d7fa4c644234a7ec9a6130b5ec903f3ef7bdb
Aja Singer <ajasinger@gmail.com>
2024-04-27 12:00:04 -0400

    map array & style

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,12 +1,23 @@
+import { techStack } from '@/lib/data';
 
 export default function Header() {
   return (
-    <header className="py-16 flex flex-col gap-10">
+    <header className="py-16 flex flex-col gap-8">
       <h1 className="text-[250px] font-semibold">Hi, I'm Aja</h1>
       <h2 className="text-3xl font-thin max-w-4xl leading-relaxed">
         a junior<span className="font-serif"> full-stack engineer </span>building at the intersection of 
         <span className="font-serif"> technology </span>and<span className="font-serif"> creativity</span>.
       </h2>
+      <ul className="flex gap-8 font-light text-lg">
+        {techStack.map(tech => (
+          <li
+            key={tech.id}
+            className="flex gap-8"
+          >
+            {tech.name}<span className={`${tech.id === techStack.length-1 && "invisible"}`}>/</span>
+          </li>
+        ))}
+      </ul>
       
     </header>
   )
