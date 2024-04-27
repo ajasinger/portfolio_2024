9dfb0c64f1a9952011cea4255825a02fda6bb66b
Aja Singer <ajasinger@gmail.com>
2024-04-27 07:04:36 -0400

    styling

--- components/Header.tsx
+++ components/Header.tsx
@@ -5,20 +5,25 @@ import Link from 'next/link';
 
 export default function Header() {
   return (
-    <header>
+    <header className="font-sans text-lg py-8">
       <nav>
-        <ul>
+        <ul className="flex gap-10 justify-end items-center">
           {links.map((link) => (
             <li 
               key={link.id}
+              className="hover:underline underline-offset-8 decoration-1"
             >
-              <Link 
-                href={link.href}
-              >
-                {link.name}
-              </Link>
+              <Link href={link.href}>{link.name}</Link>
             </li>
           ))}
+          <li>
+            <Link 
+              href={'/contact-me'}
+              className="px-6 py-3 border border-bg-stone-50 rounded-full hover:bg-stone-50 hover:text-zinc-950"
+            >
+              CONTACT ME
+            </Link>
+            </li>
         </ul>
       </nav>
     </header>
