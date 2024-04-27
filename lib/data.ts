d57eed334abab71422f0546e21bc56903535d425
Aja Singer <ajasinger@gmail.com>
2024-04-27 11:59:51 -0400

    update data

--- lib/data.ts
+++ lib/data.ts
@@ -1,17 +1,44 @@
 export const navLinks = [
     {
         name:"ABOUT ME",
-        id:1,
+        id:0,
         href:"#about"
     },
     {
         name:"WORK",
-        id:2,
+        id:1,
         href:"#work"
     },
     {
         name:"LINKEDIN",
-        id:3,
+        id:2,
         href:"https://www.linkedin.com/in/ajasinger/"
+    },
+]
+
+export const techStack = [
+    {
+        name: "NEXT.JS",
+        id: 0
+    },
+    {
+        name: "REACT",
+        id: 1
+    },
+    {
+        name: "JAVASCRIPT",
+        id: 2
+    },
+    {
+        name: "TYPESCRIPT",
+        id: 3
+    },
+    {
+        name: "TAILWIND",
+        id: 4
+    },
+    {
+        name: "CSS",
+        id: 5
     },
 ]
