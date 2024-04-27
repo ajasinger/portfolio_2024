2c93b2d2c0e860770db7742b22176dc5b7fa76d5
Aja Singer <ajasinger@gmail.com>
2024-04-27 06:23:42 -0400

    remove styling

--- app/layout.tsx
+++ app/layout.tsx
@@ -7,7 +7,7 @@ const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
   title: "Aja Singer",
-  description: "Aja Singer is a junior software engineeer building creative technology products.",
+  description: "Aja Singer is a software engineeer building at the intersection of technology and creativity.",
 };
 
 export default function RootLayout({
@@ -18,8 +18,6 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={`${inter.className} bg-gray-50 text-gray-950 relative`}>
-        <div className="bg-[#fbe2e3] absolute -z-10 top-[-6rem] right-[11rem] h-[31.25rem] w-[31.25rem] rounded-full blur-[10rem] sm:w-[68.75rem]"></div>
-        <div className="bg-[#dbd7fb] absolute -z-10 top-[-1rem] left-[-35rem] h-[31.25rem] w-[50rem] rounded-full blur-[10rem] sm:w-[68.75rem] md:left-[-33rem] lg:left-[-28rem] xl:left-[-15rem] 2xl:left-[-5rem]"></div>
         < Header />
         {children}
       </body>
