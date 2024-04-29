afdd8fe7c200b4a325d5234abae16d6756ca8641
Aja Singer <ajasinger@gmail.com>
2024-04-29 16:49:18 -0400

    add alt text

--- lib/data.ts
+++ lib/data.ts
@@ -48,12 +48,14 @@ export const projects = [
         id: 0,
         href: "/images/silvershield_site.png",
         title: "Silvershield.ai", 
+        alt: "Silvershield website landing page",
         body: "A full-stack web app built with Next.js, Tailwind CSS and Supabase for AI-enabled fraud detection."
     },
     {
         id: 1,
         href: "/images/visiblework_site.png",
         title: "Visible Work", 
+        alt: "Visible Work website landing page",
         body: "A JavaScript website for a personal branding service."
     },
 ]
