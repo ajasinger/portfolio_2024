a3b229d17610e216ac3f0b26eba5c361266e9f35
Aja Singer <ajasinger@gmail.com>
2024-04-30 12:31:20 -0400

    crete group and add hover animation

--- components/Work.tsx
+++ components/Work.tsx
@@ -32,9 +32,9 @@ export default function Work() {
                             <div className="bg-[#E0FEAF] -z-10 absolute top-[-15rem] right-[20rem] h-[31.25rem] w-[31.25rem] rounded-full blur-[7rem] sm:w-[68.75rem]"></div>
                             <div className="bg-[#EDCEE2] -z-10 absolute top-[15rem] right-[-25rem] h-[31.25rem] w-[50rem] rounded-full blur-[7rem]"></div>
                         </div>
-                        <Link href={'page'} className="text-cream text-lg flex gap-2 items-center">
+                        <Link href={'page'} className="text-cream text-lg flex gap-2 items-center group">
                             LEARN MORE
-                            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1} stroke="currentColor" className="w-10 h-10">
+                            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1} stroke="currentColor" className="w-10 h-10 transition ease-in-out delay-50 group-hover:translate-x-2">
                                 <path strokeLinecap="round" strokeLinejoin="round" d="M17.25 8.25 21 12m0 0-3.75 3.75M21 12H3" />
                             </svg>
                         </Link>
