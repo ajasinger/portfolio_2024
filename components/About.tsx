bfc13883d07bb04ec27ead1fecd706303aaa3d49
Aja Singer <ajasinger@gmail.com>
2024-04-29 22:38:30 -0400

    update copy

--- components/About.tsx
+++ components/About.tsx
@@ -9,14 +9,12 @@ export default function About() {
         <div className="flex flex-col gap-10 z-10">
           <h3 className="text-4xl font-semibold">About me</h3>
           <div className="flex flex-col gap-2">
-            <p>If there&#39;s one thing you should know about me, it&#39;s that I really like making things. From software to businesses, I&#39;ve spent my career building.</p>
-            <p>I&#39;ve built a a startup from zero to seven figures, and a brand featured on the cover of the New York Times Style section, but it wasn&#39;t until coding that I discovered what I really love building — software that improve people&#39;s lives.</p>
-            <p>These past experiences have gifted me with:</p>
-            <ol className="ml-8">
-              <li>1. An understanding of how details fit into the larger picture — how the code I&#39;m writing serves the broader vision of a product (and business), and the importance of keeping both in your sight-line.</li>
-              <li>2. Creative thinking — when faced with a new challenge, I focus not just on how it&#39;s been done before, but look for new ways it can be achieved.</li>
-            </ol>
-            <p>After writing my first few lines of code, I knew it was how I wanted to spend my days. The speed at which you can bring ideas to life and the satisfaction of bug fixing turned a week-long coding experiment into a new career.</p>
+            <p>I like making things, but I love learning even more.</p>
+            <p>I built a fashion startup from zero to seven figures featured on the cover of the New York Times Style section, but you can only innovate and iterate so much with women&#39;s workwear. </p>
+            <p>I got bored, so I sought a new challenge that satisfied my desire to make — but in an area where the potential to learn was uncapped. My husband, who works with startups, suggested software development. 
+              I took a week-long Codeacademy class and I was immediately hooked. It was the most engaged I had been professionally in quite a while — and I never looked back.</p>
+            <p>After seven months, an entrepreneur friend suggested I build a web application for a company he planned to incubate. I reminded him about my lack of experience and he responded with the most motivating, yet intimidating, words anyone has ever said to me:</p>
+            <p>“You&#39;re smart. You&#39;ll figure it out.”</p>
           </div>
         </div>
         <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1} stroke="currentColor" className="w-20 h-20 z-10">
