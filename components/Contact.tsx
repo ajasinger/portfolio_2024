40f9edaad9bb970103383405ba3be6259897a746
Aja Singer <ajasinger@gmail.com>
2024-04-29 22:08:39 -0400

    code & style component

---
+++ components/Contact.tsx
@@ -0,0 +1,19 @@
+import Link from 'next/link'
+
+export default function Contact() {
+    return(
+        <div className="px-48 py-36 bg-cream text-zinc-950 relative overflow-hidden">
+            <div className="bg-[#E0FEAF] absolute z-10 top-[-15rem] right-[20rem] h-[31.25rem] w-[31.25rem] rounded-full blur-[7rem] sm:w-[68.75rem]"></div>
+            <div className="bg-[#EDCEE2] absolute z-10 top-[15rem] right-[-25rem] h-[31.25rem] w-[50rem] rounded-full blur-[7rem]"></div>
+            <div className="flex gap-48">
+                <h3 className="text-4xl font-semibold z-50">Contact me</h3>
+                <Link 
+                    href="/dashboard"
+                    className="z-50 px-6 py-3 bg-[#C4BFFF] border border-[#C4BFFF] rounded-full hover:bg-cream text-zinc-950"
+                >
+                    Write to me
+                </Link>
+            </div>
+        </div>
+    )
+}
