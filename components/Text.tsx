6b6f887acc78f96ca64936077dcc4aab73491137
Aja Singer <ajasinger@gmail.com>
2024-04-29 21:51:15 -0400

    code & style component

---
+++ components/Text.tsx
@@ -0,0 +1,9 @@
+export default function Text() {
+    return(
+        <div className="p-16 flex justify-center">
+            <h2 className="text-3xl font-thin max-w-4xl leading-relaxed">
+                Engineering <span className="font-serif"> creative </span> solutions to <span className="font-serif"> meaningful </span> problems.
+            </h2>
+        </div>
+    )
+}
