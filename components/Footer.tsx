9df1f49874e99fa7a1ef6f63861e30767839a966
Aja Singer <ajasinger@gmail.com>
2024-04-30 11:10:29 -0400

    style padding

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -3,10 +3,10 @@ import Link from "next/link"
 export default function Footer() {
     return(
         <footer className="px-16 py-24 flex justify-between gap-16 font-sans">
-            <div className="flex flex-col gap-8 font-semibold">
-                <h2 className="text-3xl max-w-4xl leading-relaxed">AJA SINGER</h2>
-                <Link href="/work" className="text-xl">WORK</Link>
-                <Link href="/work" className="text-xl">LINKEDIN</Link>
+            <div className="flex flex-col gap-8">
+                <h2 className="text-3xl max-w-4xl leading-relaxed font-semibold">AJA SINGER</h2>
+                <Link href="#work" className="text-xl hover:underline underline-offset-8 decoration-1">WORK</Link>
+                <Link href="https://www.linkedin.com/in/ajasinger/" className="text-xl hover:underline underline-offset-8 decoration-1">LINKEDIN</Link>
             </div>
             <div className="flex flex-col gap-8">
                 <Link 
