a4a2621bfe2ae0c812ef31cb0d45e09afeef32d5
Aja Singer <ajasinger@gmail.com>
2024-04-30 13:13:56 -0400

    style component

--- app/projects/[project]/page.tsx
+++ app/projects/[project]/page.tsx
@@ -1,6 +1,6 @@
 export default function ProjectPage({ params }: { params: { project: string } }) {
     return(
-        <div>
+        <div className="bg-cream px-16 py-24 text-zinc-950 font-sans">
             <h2>{params.project}</h2>
         </div>
     )
