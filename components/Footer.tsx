e76d17c88d52fa37d8d9efd87d108b9c8a611c27
Aja Singer <ajasinger@gmail.com>
2024-04-30 14:04:23 -0400

    add home link to footer logo

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -4,7 +4,7 @@ export default function Footer() {
     return(
         <footer className="px-16 py-24 flex justify-between gap-16 font-sans">
             <div className="flex flex-col gap-8">
-                <h2 className="text-3xl max-w-4xl font-semibold">AJA SINGER</h2>
+                <Link href="/"><h2 className="text-3xl max-w-4xl font-semibold">AJA SINGER</h2></Link>
                 <Link href="/#work" className="text-xl hover:underline underline-offset-8 decoration-1">WORK</Link>
                 <Link href="https://www.linkedin.com/in/ajasinger/" className="text-xl hover:underline underline-offset-8 decoration-1">LINKEDIN</Link>
             </div>
