ce5983883b4f55d2e80de05172f991f72e375da1
Aja Singer <ajasinger@gmail.com>
2024-04-30 21:04:20 -0400

    add about data

--- lib/data.ts
+++ lib/data.ts
@@ -31,3 +31,37 @@ export const projects = [
         param: "visiblework"
     },
 ]
+
+export const about = [
+    {
+        name: "card-1",
+        body: [
+            "I like making things, but I love learning even more.", 
+            "I built a fashion startup from zero to seven figures featured on the cover of the New York Times Style section, but you can only innovate and iterate so much with women's workwear.", 
+            "I got bored, so I sought a new challenge that satisfied my desire to make—but in an area where the potential to learn was uncapped. My husband, who works with startups, suggested software development. I took a week-long Codeacademy class and I was immediately hooked. It was the most engaged I had been professionally in quite a while—and I never looked back."
+        ]
+    },
+    {
+        name: "card-2",
+        body: [
+            "After seven months, an entrepreneur friend suggested I build a web application for a company he planned to incubate. I reminded him about my lack of experience and he responded with the most motivating, yet intimidating, words anyone has ever said to me:",
+            "“You're smart. You'll figure it out.”",
+            "That was last summer.",
+            "Today, after countless hours of debugging alongside professional developers, too many Stack Overflow rabbit holes to count, and late nights pouring over documentation, I can say that I'm a freelance full-stack developer who was paid to build an AI-powered web app with Next.js, Tailwind, and Supabase.",
+            "I'm excited to join a company that values learning and creativity, where I can make a meaningful difference to a collaborative team."
+        ]
+    },
+    {
+        name: "card-2",
+        body: [
+            "A little more about me…"
+        ],
+        list: [
+            "I live in Brooklyn with my triathlete husband and inquisitive toddler—who knows that when I put my anti-fatigue glasses on and turn on the big screen, “Mommy's working”.",
+            "I'm from Toronto, which means you'll likely hear me say “sorry,” but not “eh.”",
+            "I learned to ride a bike two years ago (a learning curve about as steep as learning to code).",
+            "I think Rubirosa's vodka pizza is the best pizza in NY. (I will not be taking questions.)",
+            "When I'm not coding you'll find me trying to master a crow pose, snapping Polaroids, and seeking fresh kimchi."
+        ]
+    },
+]
