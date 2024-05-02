728cc02c1c7a19cf3a50e0930a30c8d5bf78433c
Aja Singer <ajasinger@gmail.com>
2024-05-02 16:27:09 -0400

    update content in objects

--- lib/projects-data.ts
+++ lib/projects-data.ts
@@ -2,11 +2,50 @@ export const projects = [
     { 
         param: "silvershield",
         name: "Silvershield",
-
+        overview: [
+            "A full-stack AI-enabled web application, that enables family members to monitor their senior relative's inboxes for fraud.",
+            "I built the platform from concept to beta launch and collaborated with the CEO, machine learning developers, designer, and marketing lead to develop the product and iterate. Silvershield is now in use by beta testers."
+        ],
+        overviewList: {
+            title: "The platform's functionality includes:",
+            list: [
+                "Integration with Supabase auth and database",
+                "CRUD operations for user profiles, and organizations with multiple members",
+                "Row Level Security and trigger functions with PostgresSQL",
+                "Automated emails with Sendgrid",
+                "Communication with multiple APIs and SDKs",
+                "Fully responsive frontend"
+            ]
+        },
+        websiteURL: "https://www.silvershield.ai/",
+        techStack: ["Next.JS", "Supabase", "Tailwind", "PosgresSQL", "Nylas", "Sendgrid", "Google APIs"],
+        Approach: [
+            "placeholder"
+        ]
     },
     { 
         param: "visiblework",
         name: "Visible Work",
+        overview: [
+            "A full-stack AI-enabled web application, that enables family members to monitor their senior relative's inboxes for fraud.",
+            "I built the platform from concept to beta launch and collaborated with the CEO, machine learning developers, designer, and marketing lead to create and iterate on the product. Silvershield is now in use by beta testers."
+        ],
+        overviewList: {
+            title: "The platform's functionality includes:",
+            list: [
+                "Integration with Supabase auth and database",
+                "CRUD operations for user profiles, and organizations with multiple members",
+                "Row Level Security and trigger functions with PostgresSQL",
+                "Automated emails with Sendgrid",
+                "Communication with multiple APIs and SDKs",
+                "Fully responsive frontend"
+            ]
+        },
+        websiteURL: "https://www.silvershield.ai/",
+        techStack: ["Next.JS", "Supabase", "Tailwind", "PosgresSQL", "Nylas", "Sendgrid", "Google APIs"],
+        Approach: [
+            "placeholder"
+        ]
 
     },
 ]
