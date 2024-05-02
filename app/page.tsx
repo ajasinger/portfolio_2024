7ca8a1ce5ea0c75b9b7787169e8a789b5475091e
Aja Singer <ajasinger@gmail.com>
2024-05-02 09:31:30 -0400

    import & comment out components

--- app/page.tsx
+++ app/page.tsx
@@ -3,12 +3,14 @@ import Header from "@/components/Header";
 import Work from "@/components/Work";
 import Text from "@/components/Text";
 import Contact from "@/components/Contact";
+import Carousel from "@/components/Carousel";
 
 export default function Home() {
   return (
     <main className="font-sans">
       < Header />
-      < About />
+      {/* < About /> */}
+      < Carousel />
       < Work />
       {/* < Text /> */}
       < Contact />
