6f8fc7bfbab846d5dfa42992a6814e0f7fcc624f
Aja Singer <ajasinger@gmail.com>
2024-05-03 07:37:22 -0400

    update type definition

--- app/projects/[project]/page.tsx
+++ app/projects/[project]/page.tsx
@@ -9,10 +9,16 @@ type ProjectType = {
     overviewList?: {
         title: string;
         list: string[];
-      };
+    };
     websiteURL: string;
     techStack: string[];
-    Approach: string[];
+    approach: Approach[];
+};
+
+type Approach = {
+    title: string;
+    body: string[];
+    bodyList?: { title: string; body: string }[];
 };
 
 export default function ProjectPage({ params }: { params: { project: string } }) {
@@ -24,6 +30,7 @@ export default function ProjectPage({ params }: { params: { project: string } })
         <div className="bg-cream px-16 py-24 text-zinc-950 font-sans">
             <div className="flex flex-col gap-16">
                 <h3 className="text-4xl font-semibold">{data?.name}</h3>
+                {/* Intro Section */}
                 <div className="flex flex-col gap-4 w-1/2">
                     <h4 className="font-serif text-lg font-semibold">Project Overview</h4>
                     {data?.overview?.map((text, index) => (
@@ -32,11 +39,15 @@ export default function ProjectPage({ params }: { params: { project: string } })
                         </div>
                     ))}
                     <p>{data?.overviewList?.title}</p>
+                    <div className="flex flex-col gap-2">
                     {data?.overviewList?.list?.map((text, index) => (
+                    
                         <ol key={index} className="list-disc ml-8">
                             <li>{text}</li>
                         </ol>
+                        
                     ))}
+                    </div>
                 </div>
                 <div className="flex flex-col gap-4">
                     <h4 className="font-serif text-lg font-semibold">Tech Stack</h4>
@@ -48,6 +59,7 @@ export default function ProjectPage({ params }: { params: { project: string } })
                         ))}
                     </ul>
                 </div>
+                {/* Product Overview */}
 
             </div>
         </div>
