a8c2fd93af1db9496b8a934e7b30bec855f0e37c
Aja Singer <ajasinger@gmail.com>
2024-05-03 07:37:43 -0400

    change copy

--- components/Header.tsx
+++ components/Header.tsx
@@ -5,7 +5,7 @@ export default function Header() {
     <header className="px-16 pt-8 pb-16 flex flex-col gap-8">
       <h1 className="text-[250px] font-semibold">Hi, I'm Aja</h1>
       <h2 className="text-3xl font-thin max-w-4xl leading-relaxed">
-        a junior<span className="font-serif"> full-stack engineer </span>building at the intersection of 
+        a junior<span className="font-serif"> front-end engineer </span>building at the intersection of 
         <span className="font-serif"> technology </span>and<span className="font-serif"> creativity</span>.
       </h2>
       <ul className="flex gap-8 font-light text-lg">
