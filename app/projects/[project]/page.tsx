d5efa020d17a176c3cb45904f7c141bb4b2f4ec3
Aja Singer <ajasinger@gmail.com>
2024-05-04 11:54:37 -0400

    add Image

--- app/projects/[project]/page.tsx
+++ app/projects/[project]/page.tsx
@@ -1,10 +1,13 @@
 'use client';
 
+import Image, { ImageProps } from 'next/image';
 import { projects } from '@/lib/projects-data';
 
 type ProjectType = {
     param: string;
     name: string;
+    headerHref: string;
+    headerImgAlt: string;
     overview: string[];
     overviewList?: {
         title: string;
@@ -31,22 +34,34 @@ export default function ProjectPage({ params }: { params: { project: string } })
             <div className="flex flex-col gap-16">
                 <h3 className="text-4xl font-semibold">{data?.name}</h3>
                 {/* Intro Section */}
-                <div className="flex flex-col gap-4 w-1/2">
+                <div className="flex flex-col gap-4">
                     <h4 className="font-serif text-lg font-semibold">Project Overview</h4>
-                    {data?.overview?.map((text, index) => (
-                        <div key={index}>
-                            <p>{text}</p>
+                    <div className="flex gap-16 justify-between w-full">
+                        <div className="w-2/3 flex flex-col gap-2">
+                            {data?.overview?.map((text, index) => (
+                                <div key={index}>
+                                    <p>{text}</p>
+                                </div>
+                            ))}
+                            <p>{data?.overviewList?.title}</p>
+                            <div className="flex flex-col gap-2">
+                            {data?.overviewList?.list?.map((text, index) => (
+                                <ol key={index} className="list-disc ml-8">
+                                    <li>{text}</li>
+                                </ol>
+                            ))}
+                            </div>
                         </div>
-                    ))}
-                    <p>{data?.overviewList?.title}</p>
-                    <div className="flex flex-col gap-2">
-                    {data?.overviewList?.list?.map((text, index) => (
-                    
-                        <ol key={index} className="list-disc ml-8">
-                            <li>{text}</li>
-                        </ol>
-                        
-                    ))}
+                        <div className="rounded-lg z-0 relative overflow-hidden space-y-8 h-full transition ease-in-out duration-300 delay-100 hover:scale-105">
+                            <div className="h-[425px] overflow-y-scroll">
+                                <Image
+                                    src={data?.headerHref}
+                                    width={200}
+                                    height={600}
+                                    alt={data?.headerImgAlt}
+                                />
+                            </div>
+                        </div>
                     </div>
                 </div>
                 <div className="flex flex-col gap-4">
