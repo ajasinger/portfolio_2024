2bd8c4951f0470103ceb262f53b4664e8d7ba9c8
Aja Singer <ajasinger@gmail.com>
2024-05-06 16:47:06 -0400

    style padding and add new section

--- app/projects/[project]/page.tsx
+++ app/projects/[project]/page.tsx
@@ -59,7 +59,6 @@ export default function ProjectPage({ params }: { params: { project: string } })
                     <div className="rounded-lg z-0 relative overflow-hidden space-y-8 h-full transition ease-in-out duration-300 delay-100 hover:scale-105">
                         <div className="h-[450px] overflow-y-scroll border-8 border-black">
                             {data && 
-                            // THIS IS TO GET RID OF TS ERROR -- CHANGE LATER!!!
                                 <Image
                                     src={data?.headerHref}
                                     width={600}
@@ -71,7 +70,7 @@ export default function ProjectPage({ params }: { params: { project: string } })
                     </div>
                 </div>
                 {/* Tech Stack */}
-                <div className="flex flex-col gap-4">
+                <div className="flex flex-col gap-4 z-50">
                     <h4 className="font-serif text-lg font-semibold">Tech Stack</h4>
                     <ul className="flex gap-8">
                         {data?.techStack?.map((text, index) => (
@@ -84,14 +83,53 @@ export default function ProjectPage({ params }: { params: { project: string } })
             </div>
 
             {/* Approach */}
-            <div className="px-16 py-24 bg-cream text-zinc-950 relative overflow-hidden">
-                <div className="bg-[#E0FEAF] absolute top-[-15rem] right-[20rem] h-[31.25rem] w-[31.25rem] rounded-full blur-[7rem] sm:w-[68.75rem] z-0"></div>
-                <div className="bg-[#EDCEE2] absolute top-[15rem] right-[-25rem] h-[31.25rem] w-[50rem] rounded-full blur-[7rem] z-0"></div>
+            <div className="px-16 py-12 bg-cream text-zinc-950 relative">
+                <div className="bg-[#E0FEAF] absolute top-[3rem] right-[20rem] h-[31.25rem] w-[31.25rem] rounded-full blur-[7rem] sm:w-[68.75rem] z-0"></div>
+                <div className="bg-[#EDCEE2] absolute top-[25rem] right-[-5rem] h-[31.25rem] w-[50rem] rounded-full blur-[7rem] z-0"></div>
                 { data && 
                     <div className="z-50">
                         < Accordion data={data?.approach} />
                     </div>
                 }
+            </div>
+
+            {/* Laptop Images */}
+            {/* <div className="px-16 py-12 bg-cream text-zinc-950">
+
+            </div> */}
+
+            {/* Problems & Solutions */}
+            <div className="px-16 py-12  text-zinc-950 flex gap-16 z-10">
+            <div className="rounded-lg z-0 relative overflow-hidden space-y-8 h-full transition ease-in-out duration-300 delay-100 hover:scale-105">
+                        <div className="h-[450px] overflow-y-scroll border-8 border-black">
+                            {data && 
+                                <Image
+                                    src={data?.headerHref}
+                                    width={600}
+                                    height={600}
+                                    alt={data?.headerImgAlt}
+                                />
+                            }
+                        </div>
+                    </div>
+                <div className="flex flex-col gap-4">
+                    <h4 className="font-serif text-lg font-semibold">Problems & Solutions</h4>
+                    <div className="flex flex-col gap-2">
+                        {data?.overview?.map((text, index) => (
+                            <div key={index}>
+                                <p>{text}</p>
+                            </div>
+                        ))}
+                        <p>{data?.overviewList?.title}</p>
+                        <div className="flex flex-col gap-2">
+                            {data?.overviewList?.list?.map((text, index) => (
+                                <ol key={index} className="list-disc ml-8">
+                                    <li>{text}</li>
+                                </ol>
+                            ))}
+                        </div>
+                    </div>
+                </div>
             </div>
         </div>
     )
