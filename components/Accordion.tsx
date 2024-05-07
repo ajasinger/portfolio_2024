6169559f23ba923747f14f14185050ccc154bccd
Aja Singer <ajasinger@gmail.com>
2024-05-07 15:58:11 -0400

    style padding

--- components/Accordion.tsx
+++ components/Accordion.tsx
@@ -28,7 +28,7 @@ export default function Accordion({ data }: Props) {
                                         className={`w-full bg-transparent text-zinc-950 text-lgflex flex-col gap-2 ${data?.length-1 > 1 && "min-h-[525px]"}`}
                                         onClick={() => setIsOpen(index)}
                                     >
-                                        <div className="flex flex-col gap-2 text-left p-16">
+                                        <div className="flex flex-col gap-4 text-left p-16">
                                             {data?.length-1 < 2 ? (
                                                 <h4 className="font-serif text-lg font-semibold z-50">Approach</h4>
                                             ) : (
