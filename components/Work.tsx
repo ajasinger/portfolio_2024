ebcdd71326e284ba1c553ccdecc3d4afec4cd4f5
Aja Singer <ajasinger@gmail.com>
2024-05-08 16:22:51 -0400

    add shadow

--- components/Work.tsx
+++ components/Work.tsx
@@ -18,7 +18,7 @@ export default function Work() {
                             href={`projects/${project.param}`}
                             className=" bg-cream p-8 z-0 relative overflow-hidden space-y-8 h-full transition ease-in-out duration-300 delay-100 hover:scale-105"
                         >
-                            <div className="h-[325px] overflow-y-scroll">
+                            <div className="h-[325px] overflow-y-scroll shadow-[0px_4px_16px_rgba(17,17,26,0.1),_0px_8px_24px_rgba(17,17,26,0.1),_0px_16px_56px_rgba(17,17,26,0.1)]">
                                 <Image
                                     src={project.href}
                                     width={600}
