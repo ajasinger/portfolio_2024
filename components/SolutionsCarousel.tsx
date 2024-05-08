65a0ca264b7051ab5b382be6582617a68b360e67
Aja Singer <ajasinger@gmail.com>
2024-05-08 07:04:39 -0400

    styling

--- components/SolutionsCarousel.tsx
+++ components/SolutionsCarousel.tsx
@@ -15,18 +15,18 @@ export default function SolutionsCarousel({ data }: Props) {
     const [isOpen, setIsOpen] = useState(0);
 
     return(
-        <div className="flex flex-col gap-4">
+        <div>
             {data?.map((text, index) => (
                 <div key={index}>
                     {isOpen === index &&
-                        <div className="flex flex-col gap-4">
+                        <div>
                             <h5 className="font-semibold uppercase text-left">{text.title}</h5>
                             <p>{text.body}</p>
                         </div>
                     }
                 </div>
             ))}
-            <div className="flex gap-4">
+            <div className="flex gap-4 justify-center pt-6">
                 {data?.map((_, index) => (
                     <button 
                         key={index}
