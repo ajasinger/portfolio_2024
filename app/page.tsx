d683ea0a87589d17fdd179dc99273d6762e4e06a
Aja Singer <ajasinger@gmail.com>
2024-05-08 21:13:27 -0400

    import about section accordion

--- app/page.tsx
+++ app/page.tsx
@@ -4,11 +4,13 @@ import Work from "@/components/Work";
 import Text from "@/components/Text";
 import Contact from "@/components/Contact";
 import Carousel from "@/components/Carousel";
+import AboutAccordion from "@/components/AboutAccordion";
 
 export default function Home() {
   return (
     <main className="font-sans">
       < Header />
+      < AboutAccordion />
       {/* < About />
       < Carousel /> */}
       < Work />
