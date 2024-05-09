66c86ad248bb376f7a11b5236a07edce4dc5baa0
Aja Singer <ajasinger@gmail.com>
2024-05-08 21:12:38 -0400

    add github

--- lib/data.ts
+++ lib/data.ts
@@ -11,6 +11,10 @@ export const navLinks = [
         name:"LINKEDIN",
         href:"https://www.linkedin.com/in/ajasinger/"
     },
+    {
+        name:"GITHUB",
+        href:"https://github.com/ajasinger"
+    }
 ]
 
 export const techStack = [{ name: "NEXT.JS" }, { name: "REACT" }, { name: "JAVASCRIPT" }, { name: "TYPESCRIPT" }, { name: "TAILWIND" }, { name: "CSS" } ]
@@ -32,42 +36,42 @@ export const projects = [
     },
 ]
 
+// export const aboutCards = [
+//     {
+//         name: "card-1",
+//         body: [
+//             "I like making things, but I love learning even more.", 
+//             "I built a fashion startup from zero to seven figures and a brand featured on the cover of the New York Times Style section, but you can only innovate so much with women's workwear.", 
+//             "I got bored, so I looked for a new challenge to satisfy my desire to build — one where the potential to learn was uncapped. My husband, who works with startups, suggested software development.",
+//             "Two days into my Codecademy course, I was hooked."
+//         ]
+//     },
+//     {
+//         name: "card-2",
+//         body: [
+//             "Seven months later, a conversation about my career change led to my first freelance project — a web application for a company an entrepreneur friend planned to incubate. Despite the fact that I hadn't yet built a full-stack web app, he was confident: ",
+//             "“You're smart. You'll figure it out.”",
+//             "And I did.",
+//             "Today, after countless hours of debugging alongside professional developers, Stack Overflow rabbit holes, and late nights pouring over documentation, I can say that I'm a freelance full-stack developer who was paid to build an AI-powered web app with Next.js, Tailwind, and Supabase.",
+//         ]
+//     },
+//     {
+//         name: "card-3",
+//         body: [
+//             "Now, I'm excited to join a company that values learning and creativity, where I can collaborate and make a meaningful difference.",
+//             "A little more about me:"
+//         ],
+//         list: [
+//             "I'm from Toronto, which means you'll likely hear me say “sorry,” but not “eh.”",
+//             "I think Rubirosa's vodka pizza is the best pizza in NY. (I will not be taking questions.)",
+//             "When I'm not coding you'll find me trying to master a crow pose, snapping Polaroids, and seeking out fresh kimchi."
+//         ]
+//     },
+// ]
+
 export const aboutCards = [
     {
-        name: "card-1",
-        body: [
-            "I like making things, but I love learning even more.", 
-            "I built a fashion startup from zero to seven figures and a brand featured on the cover of the New York Times Style section, but you can only innovate so much with women's workwear.", 
-            "I got bored, so I looked for a new challenge to satisfy my desire to build — one where the potential to learn was uncapped. My husband, who works with startups, suggested software development.",
-            "Two days into my Codecademy course, I was hooked."
-        ]
-    },
-    {
-        name: "card-2",
-        body: [
-            "Seven months later, a conversation about my career change led to my first freelance project — a web application for a company an entrepreneur friend planned to incubate. Despite the fact that I hadn't yet built a full-stack web app, he was confident: ",
-            "“You're smart. You'll figure it out.”",
-            "And I did.",
-            "Today, after countless hours of debugging alongside professional developers, Stack Overflow rabbit holes, and late nights pouring over documentation, I can say that I'm a freelance full-stack developer who was paid to build an AI-powered web app with Next.js, Tailwind, and Supabase.",
-        ]
-    },
-    {
-        name: "card-3",
-        body: [
-            "Now, I'm excited to join a company that values learning and creativity, where I can collaborate and make a meaningful difference.",
-            "A little more about me:"
-        ],
-        list: [
-            "I'm from Toronto, which means you'll likely hear me say “sorry,” but not “eh.”",
-            "I think Rubirosa's vodka pizza is the best pizza in NY. (I will not be taking questions.)",
-            "When I'm not coding you'll find me trying to master a crow pose, snapping Polaroids, and seeking out fresh kimchi."
-        ]
-    },
-]
-
-export const aboutCards1 = [
-    {
-        name: "card-1",
+        title: "About me",
         body: [
             "I like making things, but I love learning even more.", 
             "I built a fashion startup from zero to seven figures and a brand featured on the cover of the New York Times Style section, but you can only innovate so much with women's workwear.", 
@@ -77,16 +81,15 @@ export const aboutCards1 = [
             "“You're smart. You'll figure it out.”",
             "And I did.",
             "Today, after countless hours of debugging alongside professional developers, Stack Overflow rabbit holes, and late nights pouring over documentation, I can say that I'm a freelance full-stack developer who was paid to build an AI-powered web app with Next.js, Tailwind, and Supabase.",
+            "Now, I'm excited to join a company that values learning and creativity, where I can collaborate and make a meaningful difference.",
         ]
     },
     {
-        name: "card-2",
-        body: [
-            "Now, I'm excited to join a company that values learning and creativity, where I can collaborate and make a meaningful difference.",
-            "A little more about me:"
-        ],
+        title: "More about me",
         list: [
+            "I live in Brooklyn with my triathlete husband and inquisitive toddler—who knows that when I put my glasses on and turn on the big screen, “Mommy's working”.",
             "I'm from Toronto, which means you'll likely hear me say “sorry,” but not “eh.”",
+            "I just learned to ride a bike two years ago.",
             "I think Rubirosa's vodka pizza is the best pizza in NY. (I will not be taking questions.)",
             "When I'm not coding you'll find me trying to master a crow pose, snapping Polaroids, and seeking out fresh kimchi."
         ]
