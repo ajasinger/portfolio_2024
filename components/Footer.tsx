cc49443a76fbd6266ec5f165fdb46940baa4d4a6
Aja Singer <ajasinger@gmail.com>
2024-05-11 06:11:28 -0400

    comment out out Work link

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -5,7 +5,7 @@ export default function Footer() {
         <footer className="px-16 py-24 flex justify-between gap-16 font-sans">
             <div className="flex flex-col gap-8">
                 <Link href="/"><h2 className="text-3xl max-w-4xl font-semibold">AJA SINGER</h2></Link>
-                <Link href="/#work" className="text-xl hover:underline underline-offset-8 decoration-1">WORK</Link>
+                {/* <Link href="/#work" className="text-xl hover:underline underline-offset-8 decoration-1">WORK</Link> */}
                 <Link href="https://www.linkedin.com/in/ajasinger/" className="text-xl hover:underline underline-offset-8 decoration-1" target="_blank">LINKEDIN</Link>
                 <Link href="https://github.com/ajasinger" className="text-xl hover:underline underline-offset-8 decoration-1" target="_blank">GITHUB</Link>
             </div>
