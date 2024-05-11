4e5bedaf71795a74aad90c0edd65b5e8117373ae
Aja Singer <ajasinger@gmail.com>
2024-05-11 14:38:21 -0400

    rseponsive styling

--- components/Header.tsx
+++ components/Header.tsx
@@ -2,13 +2,13 @@ import { techStack } from '@/lib/data';
 
 export default function Header() {
   return (
-    <header className="px-16 pt-8 pb-24 flex flex-col gap-8">
-      <h1 className="text-[250px] font-semibold">Hi, I'm Aja</h1>
-      <h2 className="text-3xl font-thin max-w-4xl leading-relaxed">
+    <header className="px-8 md:px-16 pt-8 pb-24 flex flex-col gap-8">
+      <h1 className="text-[75px] sm:text-[100px] md:text-[150px] lg:text-[200px] xl:text-[250px] font-semibold">Hi, I'm Aja</h1>
+      <h2 className="text-2xl md:text-3xl font-thin max-w-4xl leading-relaxed">
         a junior<span className="font-serif"> front-end engineer </span>building at the intersection of 
         <span className="font-serif"> technology </span>and<span className="font-serif"> creativity</span>.
       </h2>
-      <ul className="flex gap-4 sm:gap-8 font-light text-lg flex-wrap">
+      <ul className="flex gap-4 sm:gap-8 font-light md:text-lg flex-wrap">
         {techStack.map((tech, index) => (
           <li
             key={index}
