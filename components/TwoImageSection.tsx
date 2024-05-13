5ce1af3b5108a98365b1d11658bee2b054bb234f
Aja Singer <ajasinger@gmail.com>
2024-05-13 08:26:49 -0400

    hover onyl above lg

--- components/TwoImageSection.tsx
+++ components/TwoImageSection.tsx
@@ -17,8 +17,8 @@ interface Props {
 export default function TwoImageSection ({ image1, image2 }: Props) {
     return(
         <div className="flex gap-16 justify-between">
-            <div className="z-10 relative group overflow-hidden overflow-y-scroll h-full transition ease-in-out duration-300 delay-100 hover:scale-105 shadow-[0px_4px_16px_rgba(17,17,26,0.1),_0px_8px_24px_rgba(17,17,26,0.1),_0px_16px_56px_rgba(17,17,26,0.1)]">
-                    <div className={`${image1?.srcHover && "group-hover:opacity-0 transition-opacity duration-300 delay-100 ease-in-out"}`}>
+            <div className="z-10 relative group overflow-hidden overflow-y-scroll h-full transition ease-in-out duration-300 delay-100 lg:hover:scale-105 shadow-[0px_4px_16px_rgba(17,17,26,0.1),_0px_8px_24px_rgba(17,17,26,0.1),_0px_16px_56px_rgba(17,17,26,0.1)]">
+                    <div className={`${image1?.srcHover && "lg:group-hover:opacity-0 transition-opacity duration-300 delay-100 ease-in-out"}`}>
                         <Image
                             src={image1?.src}
                             width={800}
@@ -27,7 +27,7 @@ export default function TwoImageSection ({ image1, image2 }: Props) {
                         />    
                     </div>
                     {image1?.srcHover && image1?.altHover &&
-                        <div className="opacity-0 group-hover:opacity-100 bg-center bg-no-repeat bg-cover absolute top-0 left-0 transition-opacity duration-300 delay-100 ease-in-out">
+                        <div className="opacity-0 lg:group-hover:opacity-100 bg-center bg-no-repeat bg-cover absolute top-0 left-0 transition-opacity duration-300 delay-100 ease-in-out">
                             <Image
                                 src={image1?.srcHover}
                                 width={800}
