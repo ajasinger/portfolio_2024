d113502d999c1a4dbafdd2c624c694732aacd8be
Aja Singer <ajasinger@gmail.com>
2024-05-13 11:17:27 -0400

    make images responsive width

--- components/Work.tsx
+++ components/Work.tsx
@@ -21,8 +21,10 @@ export default function Work() {
                             <div className="h-[400px] overflow-y-scroll shadow-[0px_4px_16px_rgba(17,17,26,0.1),_0px_8px_24px_rgba(17,17,26,0.1),_0px_16px_56px_rgba(17,17,26,0.1)]">
                                 <Image
                                     src={project.href}
+                                    objectFit="cover"
                                     width={600}
                                     height={600}
+                                    className="w-full top-0 left-0"
                                     alt={project.alt}
                                 />
                             </div>
