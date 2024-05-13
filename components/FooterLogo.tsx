dc18f3b726c042c21492150307fb874e67670a50
Aja Singer <ajasinger@gmail.com>
2024-05-13 12:35:37 -0400

    delete

--- components/FooterLogo.tsx
+++
@@ -1,7 +0,0 @@
-export default function FooterLogo(){
-    return(
-        <div className="font-sans w-full flex justify-center">
-            <p className="text-[190px] font-semibold text-nowrap w-screen text-center align-text-bottom leading-none">AJA SINGER</p>
-        </div>
-    )
-}
