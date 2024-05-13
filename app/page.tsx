348ba8382e1425d584d0875117ab772026d6ce75
Aja Singer <ajasinger@gmail.com>
2024-05-13 14:30:18 -0400

    style color blur for mobile

--- app/page.tsx
+++ app/page.tsx
@@ -8,8 +8,8 @@ export default function Home() {
     <main className="font-sans">
       < Header />
       <div className="px-8 md:px-16 py-12 sm:py-24 bg-cream text-zinc-950 relative overflow-hidden">
-        <div className="bg-[#E0FEAF] absolute top-[-15rem] right-[20rem] h-[31.25rem] w-[31.25rem] rounded-full blur-[7rem] sm:w-[68.75rem] z-0"></div>
-        <div className="bg-[#EDCEE2] absolute top-[25rem] right-[0rem] h-[31.25rem] w-[50rem] rounded-full blur-[7rem] z-0"></div>
+        <div className="bg-[#E0FEAF] absolute top-[-5rem] right-[0rem] sm:top-[-15rem] sm:right-[20rem] h-[31.25rem] w-[31.25rem] rounded-full blur-[3rem] sm:blur-[7rem] z-0"></div>
+        <div className="bg-[#EDCEE2] absolute top-[15rem] right-[0rem] sm:top-[25rem] sm:right-[0rem] h-[31.25rem] w-[50rem] rounded-full blur-[7rem] z-0"></div>
           <div className="z-50">
             < AboutAccordion />
           </div>
