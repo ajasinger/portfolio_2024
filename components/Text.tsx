0e694505312d9e010695281f76c87968f46e1340
Aja Singer <ajasinger@gmail.com>
2024-05-13 13:12:17 -0400

    text center mobile

--- components/Text.tsx
+++ components/Text.tsx
@@ -4,7 +4,7 @@ export default function Text() {
             <div className="bg-[#E0FEAF] absolute z-10 top-[-15rem] right-[20rem] h-[31.25rem] w-[31.25rem] rounded-full blur-[7rem] sm:w-[68.75rem]"></div>
             <div className="bg-[#EDCEE2] absolute z-10 top-[15rem] right-[-25rem] h-[31.25rem] w-[50rem] rounded-full blur-[7rem]"></div>
             <div className="flex justify-center">
-                <h2 className="text-2xl md:text-3xl font-thin leading-relaxed z-50">
+                <h2 className="text-center sm:text-left text-2xl md:text-3xl font-thin leading-relaxed z-50">
                     Engineering <span className="font-serif"> creative </span> solutions to <span className="font-serif"> meaningful </span> problems.
                 </h2>
             </div>
