67132073720da0430ee64841dff849ca881553d6
Aja Singer <ajasinger@gmail.com>
2024-05-13 07:36:27 -0400

    comment our carousel component

--- components/Carousel.tsx
+++ components/Carousel.tsx
@@ -2,27 +2,27 @@
 
 import { motion, useTransform, useScroll } from "framer-motion";
 import { useRef } from "react";
-import Card from "./Card";
-import { aboutCards } from "@/lib/data";
+// import Card from "./Card";
+// import { aboutCards } from "@/lib/data";
 
-export default function Carousel() {
-    const targetRef = useRef<HTMLDivElement | null>(null);
-    const { scrollYProgress } = useScroll({
-        target: targetRef,
-    });
+// export default function Carousel() {
+//     const targetRef = useRef<HTMLDivElement | null>(null);
+//     const { scrollYProgress } = useScroll({
+//         target: targetRef,
+//     });
 
-  const x = useTransform(scrollYProgress, [0, 1], ["1%", "-43%"]);
+//   const x = useTransform(scrollYProgress, [0, 1], ["1%", "-43%"]);
 
-    return(
-        <div ref={targetRef} className="relative h-[300vh]">
-            <div className="sticky top-0 flex h-screen items-center overflow-hidden">
-                <h3 className="absolute z-50 text-4xl text-left font-medium text-cream px-16 top-20">About me</h3>
-                <motion.div style={{ x }} className="flex gap-4">
-                    {aboutCards.map((card, index) => {
-                        return <Card card={card} index={index} />;
-                    })}
-                </motion.div>
-            </div>
-        </div>
-    )
-}
+//     return(
+//         <div ref={targetRef} className="relative h-[300vh]">
+//             <div className="sticky top-0 flex h-screen items-center overflow-hidden">
+//                 <h3 className="absolute z-50 text-4xl text-left font-medium text-cream px-16 top-20">About me</h3>
+//                 <motion.div style={{ x }} className="flex gap-4">
+//                     {aboutCards.map((card, index) => {
+//                         return <Card card={card} index={index} />;
+//                     })}
+//                 </motion.div>
+//             </div>
+//         </div>
+//     )
+// }
