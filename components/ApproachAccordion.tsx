788b2fed0474b587a6960eba60a5ec09acf3aa2d
Aja Singer <ajasinger@gmail.com>
2024-05-13 17:51:33 -0400

    reposnive styling and add mobile header buttons

--- components/ApproachAccordion.tsx
+++ components/ApproachAccordion.tsx
@@ -17,7 +17,20 @@ export default function ApproachAccordion({ data }: Props) {
 
     return(
         <div className="flex">
-            <div className="border border-zinc-950 z-50 h-full">
+            <div className="sm:border border-zinc-950 z-50 h-full">
+                {/* mobile header buttons */}
+                <div className="flex flex-wrap gap-4 pb-8 sm:hidden">
+                    {data?.map((text, index) => (
+                        <div key={index}>
+                            <button
+                                onClick={() => setIsOpen(index)}
+                                className={`${isOpen === index ? "bg-zinc-950 text-cream" : "text-zinc-950"} text-sm px-4 py-3 border border-zinc-950 rounded-full hover:bg-zinc-950 hover:text-cream uppercase text-nowrap`}
+                            >
+                                {text.title}
+                            </button>
+                        </div>
+                    ))}
+                </div>
                 <div className="flex flex-col gap-4">
                     <div className="flex divide-x-2 divide-transparent">
                         {data?.map((text, index) => (
@@ -27,7 +40,7 @@ export default function ApproachAccordion({ data }: Props) {
                                         className={`w-full bg-transparent text-zinc-950 text-lgflex flex-col gap-2 ${data?.length-1 > 1 && "min-h-[425px]"}`}
                                         onClick={() => setIsOpen(index)}
                                     >
-                                        <div className="flex flex-col gap-4 text-left p-16">
+                                        <div className="flex flex-col gap-4 text-left sm:p-16">
                                             {index === 0 ? (
                                                 <h5 className="font-semibold text-left text-xl font-serif">Approach</h5>
                                             ) : (
@@ -58,7 +71,7 @@ export default function ApproachAccordion({ data }: Props) {
                                     <button
                                         key={index}
                                         //className={`bg-zinc-950 text-cream hover:text-zinc-950  hover:backdrop-blur hover:bg-white/30 w-16 h-full flex justify-center items-center hover:border-x hover:border-zinc-950`}
-                                        className={`bg-zinc-950 hover:bg-transparent text-cream hover:text-zinc-950  w-16 h-full flex justify-center items-center hover:border-zinc-950 ${index !== data?.length-1 && "hover:border-r"} ${index !== 0 && "hover:border-l"}`}
+                                        className={`bg-zinc-950 hover:bg-transparent text-cream hover:text-zinc-950  w-16 h-full justify-center items-center hover:border-zinc-950 hidden sm:flex ${index !== data?.length-1 && "hover:border-r"} ${index !== 0 && "hover:border-l"}`}
                                         onClick={() => setIsOpen(index)}
                                     >
                                         <h5 className="font-semibold uppercase text-lg -rotate-90">{text.title}</h5>
--- components/LearningsAccordion.tsx
+++ components/LearningsAccordion.tsx
@@ -18,7 +18,20 @@ export default function LearningsAccordion({ data }: Props) {
 
     return(
         <div className="flex pb-24">
-            <div className="border border-zinc-950 z-50 h-full">
+            <div className="sm:border border-zinc-950 z-50 h-full">
+                {/* mobile header buttons */}
+                <div className="flex flex-wrap gap-4 pb-8 sm:hidden">
+                    {data?.map((text, index) => (
+                        <div key={index}>
+                            <button
+                                onClick={() => setIsOpen(index)}
+                                className={`${isOpen === index ? "bg-zinc-950 text-cream" : "text-zinc-950"} text-sm px-4 py-3 border border-zinc-950 rounded-full hover:bg-zinc-950 hover:text-cream uppercase text-nowrap`}
+                            >
+                                {text.title}
+                            </button>
+                        </div>
+                    ))}
+                </div>
                 <div className="flex flex-col gap-4">
                     <div className="flex divide-x-2 divide-transparent">
                         {data?.map((text, index) => (
@@ -28,7 +41,7 @@ export default function LearningsAccordion({ data }: Props) {
                                         className={`w-full bg-transparent text-zinc-950 text-lgflex flex-col gap-2 ${data?.length-1 > 1 && "min-h-[425px]"}`}
                                         onClick={() => setIsOpen(index)}
                                     >
-                                        <div className="flex flex-col gap-4 text-left p-16">
+                                        <div className="flex flex-col gap-4 text-left sm:p-16">
                                             {index === 0 ? (
                                                 <h5 className="font-semibold text-left font-serif text-xl">Learnings</h5>
                                             ) : (
@@ -58,7 +71,7 @@ export default function LearningsAccordion({ data }: Props) {
                                     <button
                                         key={index}
                                         //className={`bg-zinc-950 text-cream hover:text-zinc-950  hover:backdrop-blur hover:bg-white/30 w-16 h-full flex justify-center items-center hover:border-x hover:border-zinc-950`}
-                                        className={`bg-zinc-950 hover:bg-transparent text-cream hover:text-zinc-950  w-16 h-full flex justify-center items-center hover:border-zinc-950 ${index !== data?.length-1 && "hover:border-r"} ${index !== 0 && "hover:border-l"}`}
+                                        className={`bg-zinc-950 hover:bg-transparent text-cream hover:text-zinc-950  w-16 h-full hidden sm:flex justify-center items-center hover:border-zinc-950 ${index !== data?.length-1 && "hover:border-r"} ${index !== 0 && "hover:border-l"}`}
                                         onClick={() => setIsOpen(index)}
                                     >
                                         <h5 className="font-semibold uppercase text-lg -rotate-90 text-nowrap">{text.title}</h5>
