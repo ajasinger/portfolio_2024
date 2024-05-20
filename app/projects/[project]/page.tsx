4000dd85fd6b6f4adffa5381c808888413d80b90
Aja Singer <ajasinger@gmail.com>
2024-05-20 15:11:05 -0400

    remove ineffective fix

--- app/projects/[project]/page.tsx
+++ app/projects/[project]/page.tsx
@@ -134,7 +134,7 @@ export default function ProjectPage({ params }: { params: { project: string } })
         
 
             {/* Approach */}
-            <div className="px-8 md:px-16 py-12 text-zinc-950 relative bg-transparent">
+            <div className="px-8 md:px-16 py-12 text-zinc-950 relative">
                 {/* <div className="bg-[#E0FEAF] absolute top-[3rem] right-[20rem] h-[31.25rem] w-[31.25rem] rounded-full blur-[7rem] sm:w-[68.75rem] z-0"></div> */}
                 {/* <div className="bg-[#EDCEE2] absolute top-[25rem] right-[0rem] h-[31.25rem] w-[50rem] rounded-full blur-[7rem] z-0"></div> */}
                 { data && 
