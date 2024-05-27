f56441b917c1e4784cbf82f062aa25fd8a396412
Aja Singer <ajasinger@gmail.com>
2024-05-27 17:19:50 -0400

    copy updates

--- lib/data.ts
+++ lib/data.ts
@@ -94,7 +94,11 @@ export const aboutCards = [
         list: [
             // "I live in Brooklyn with my triathlete husband and inquisitive toddler—who knows that when I put my glasses on and turn on the big screen, “Mommy's working”.",
             "I'm from Toronto, which means you'll likely hear me say “sorry,” but not “eh.”",
+            "I have lived in Toronto, Montreal, and New York (for a long time) and South Africa and Australia (for a short time).",
             "I just learned to ride a bike two years ago.",
+            "I have been meditating and doing yoga on and off for 10 years",
+            "I love to draw and paint but I don't get to do it as much as I'd like.", 
+            "I got behind the wheel of an airplane before a car.",
             "I think Rubirosa's vodka pizza is the best pizza in NY. (I will not be taking questions.)",
             "When I'm not coding you'll find me trying to master a crow pose, snapping Polaroids, and seeking out fresh kimchi."
         ]
