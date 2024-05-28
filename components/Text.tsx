bdafee9eeda3f9181095d11de38780d60a96c640
Aja Singer <ajasinger@gmail.com>
2024-05-27 20:50:44 -0400

    chnage z index of color blurs

--- components/Text.tsx
+++ components/Text.tsx
@@ -20,8 +20,8 @@ export default function Text() {
 
     return(
         <div className="px-8 md:px-16 py-12 sm:py-24 bg-cream text-zinc-950 relative overflow-hidden">
-            <div className="bg-[#E0FEAF] absolute z-10 top-[-15rem] right-[20rem] h-[31.25rem] w-[31.25rem] rounded-full blur-[7rem] sm:w-[68.75rem]"></div>
-            <div className="bg-[#EDCEE2] absolute z-10 top-[15rem] right-[-25rem] h-[31.25rem] w-[50rem] rounded-full blur-[7rem]"></div>
+            <div className="bg-[#E0FEAF] absolute z-0 top-[-15rem] right-[20rem] h-[31.25rem] w-[31.25rem] rounded-full blur-[7rem] sm:w-[68.75rem]"></div>
+            <div className="bg-[#EDCEE2] absolute z-0 top-[15rem] right-[-25rem] h-[31.25rem] w-[50rem] rounded-full blur-[7rem]"></div>
             <motion.div
                 ref={ref}
                 initial="hidden"
@@ -32,11 +32,11 @@ export default function Text() {
                 }}
                 transition={{ duration: 1 }}
             >
-                <div className="flex justify-center">
-                    <h2 className="text-center sm:text-left text-2xl md:text-3xl font-thin leading-relaxed z-50">
-                        Engineering <span className="font-serif"> creative </span> solutions to <span className="font-serif"> meaningful </span> problems.
-                    </h2>
-                </div>
+            <div className="flex justify-center">
+                <h2 className="text-center sm:text-left text-2xl md:text-3xl font-thin leading-relaxed z-50">
+                    Engineering <span className="font-serif"> creative </span> solutions to <span className="font-serif"> meaningful </span> problems.
+                </h2>
+            </div>
             </motion.div>
         </div>
     )
