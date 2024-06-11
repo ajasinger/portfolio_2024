2cd605d0fe7b903f2a6097e795f28bcc1760e7f0
Aja Singer <ajasinger@gmail.com>
2024-06-11 16:18:47 -0400

    update copy

--- components/Header.tsx
+++ components/Header.tsx
@@ -14,7 +14,7 @@ export default function Header() {
       >
         <h2 className="text-2xl md:text-3xl font-thin max-w-4xl leading-relaxed">
           a<span className="font-serif"> full-stack engineer </span>and 
-          <span className="font-serif"> creative thinker</span>.
+          <span className="font-serif"> creative</span>.
         </h2>
       </motion.div>
       <motion.div
